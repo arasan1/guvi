d61270e9c407d9215adc739fcbaa95f7770b97a9
poovarasan <floweryasan123@gmail.com>
2018-08-01 10:02:14 +0530

    Create  NumericDigits.java

---
+++ NumericDigits.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class NumericDigits {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int s=str.length();
+		System.out.println(s);	
+	}
+
+}
