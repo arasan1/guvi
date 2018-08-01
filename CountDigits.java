54ebd2a0786db5ddd46c18a713ebc33597cad130
poovarasan <floweryasan123@gmail.com>
2018-08-01 14:20:10 +0530

    Create CountDigits.java

---
+++ CountDigits.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CountDigits {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		String str=Integer.toString(a);
+		int len=str.length();
+		System.out.println(len);
+	}
+}
+	
