09f0c47dac377dd3a54b2236db723c18ac47578b
poovarasan <floweryasan123@gmail.com>
2018-08-01 13:36:32 +0530

    Create Ntimes.java

---
+++ Ntimes.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ntimes {
+
+	public static void main(String[] args) {
+
+
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		String s="Hello";
+		for(int i=1;i<=a;i++) {
+			System.out.println(s);
+		}
+	}
+		
+	}
